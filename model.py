64f0daebb725cc4571f551042de5a0640fc7b6bc
David Maulick <dmaulick@users.noreply.github.com>
2025-01-06 14:30:19 -0600

    feed forward

--- model.py
+++ model.py
@@ -88,4 +88,24 @@ class LayerNormalization(nn.Module):
         std = x.std(dim = -1, keepdim= True)
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
 
+class FeedForwardBlock(nn.Module):
+    """
+    Standard feed-forward network in transformer architecture.
+    Consists of two linear transformations with a ReLU activation in between.
+    
+    Args:
+        d_model: Input/output dimension
+        d_ff: Hidden dimension of the feed-forward layer (typically 4x d_model)
+        dropout: Dropout rate
+    """
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)  # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)  # W2 and B2
+
+    def forward(self, x):
+        # (Batch, Seq_Len, d_model) --> (Batch, Seq_Len, d_ff) --> (Batch, Seq_Len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
 
